034b081549a45fec399dfb07a84cb65439d82b4f
KSRATHORE761 <rathore.kuldeep761@gmail.com>
2024-06-19 06:23:53 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,23 @@
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:'child'},
+        [
+            React.createElement("h1",{},"I am a h1 tag"),
+            React.createElement("h2",{},"I am a h2 tag")
+        ]
+    )
+);
+
+const heading = React.createElement("h1",
+    {
+        id:"heading"
+    },
+    "Hello world from react!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
+
+console.log(parent) // object;
+root.render(parent); 
